c1fe73a32a24eb46af222278e5cc0f1e3e020fa5
javarome <javarome@gmail.com>
2023-12-31 17:41:39 +0100

    Search label

--- search.js
+++ search.js
@@ -35,5 +35,5 @@ const search = document.querySelector("#main-nav .search")
 search.onmouseover = siteSearchLoad
 const searchInput = document.querySelector("#search-site")
 searchInput.disabled = false
-searchInput.setAttribute("placeHolder", "Recherche")
+searchInput.setAttribute("placeHolder", "Search")
 searchInput.oninput = (event) => siteSearchChange(event)
